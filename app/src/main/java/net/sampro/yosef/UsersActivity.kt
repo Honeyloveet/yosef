eeced71877a9bbcbd3a37e838ae75f4503493bf2
Samuel Kebede <honeyloveet@gmail.com>
2023-02-21 03:24:05 +0300

    finished user Password Changing and updated versionCode to 2 and versionName to 1.0.1

--- app/src/main/java/net/sampro/yosef/UsersActivity.kt
+++ app/src/main/java/net/sampro/yosef/UsersActivity.kt
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -19,12 +23,15 @@ import kotlinx.coroutines.withContext
 import net.sampro.yosef.adapters.UsersAdapter
 import net.sampro.yosef.databinding.ActivityUsersBinding
 import net.sampro.yosef.models.UserGet
+import net.sampro.yosef.utils.LoggedInUser
 
 class UsersActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUsersBinding
 
     private lateinit var dbReference: FirebaseDatabase
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     private lateinit var usersList: MutableList<UserGet>
     private lateinit var userAdapter: UsersAdapter
@@ -39,6 +46,8 @@ class UsersActivity : AppCompatActivity() {
         }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         usersList = mutableListOf()
         dbReference = FirebaseDatabase.getInstance()
@@ -59,7 +68,19 @@ class UsersActivity : AppCompatActivity() {
 
         userAdapter.setOnItemClickListener(object : UsersAdapter.OnItemClickListener {
             override fun onChangePasswordClick(position: Int) {
-                Toast.makeText(this@UsersActivity, "Change Password Clicked.", Toast.LENGTH_SHORT).show()
+//                val loginEmail = usersList[position].email.toString()
+//                val loginPassword = usersList[position].password.toString()
+//                firebaseAuth.signOut()
+//                firebaseAuth.signInWithEmailAndPassword(loginEmail, loginPassword)
+//                    .addOnCompleteListener {
+//                        if (it.isSuccessful) {
+//                            firebaseAuth.signOut()
+//                        } else {
+//                            Toast.makeText(this@UsersActivity, "Password Change Failed!!.", Toast.LENGTH_SHORT).show()
+//                        }
+//                    }
+//                Toast.makeText(this@UsersActivity, "Change Password Clicked.", Toast.LENGTH_SHORT).show()
+                showChangePasswordDialog(position)
             }
 
             override fun onDeleteUserClick(position: Int) {
@@ -71,6 +92,82 @@ class UsersActivity : AppCompatActivity() {
         lifecycleScope.launch(Dispatchers.IO) {
             getUsers()
         }
+    }
+
+    private fun showChangePasswordDialog(position: Int) {
+
+        val userName = usersList[position].name.toString()
+        val input = EditText(this)
+        input.hint = "Enter New Password"
+
+        val alertDialog = MaterialAlertDialogBuilder(this)
+        alertDialog.setTitle("Change $userName's Password")
+        //alertDialog.setMessage("Enter New Password")
+        alertDialog.setView(input)
+        alertDialog.setNeutralButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+        alertDialog.setPositiveButton("Save") { dialog, which ->
+            if (input.text.toString().isNotEmpty()) {
+                dialog.dismiss()
+                changePassword(position, input.text.toString())
+            } else {
+                Toast.makeText(this@UsersActivity, "Please Enter New Password.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        alertDialog.show()
+    }
+
+    private fun changePassword(position: Int, newPassword: String) {
+        binding.clLoadingUsers.visibility = View.VISIBLE
+
+        val mainUserPosition = getMainUserIndex()
+        val mainUserEmail = usersList[mainUserPosition].email.toString()
+        val mainUserPassword = usersList[mainUserPosition].password.toString()
+
+        val loginEmail = usersList[position].email.toString()
+        val loginPassword = usersList[position].password.toString()
+        firebaseAuth.signOut()
+        firebaseAuth.signInWithEmailAndPassword(loginEmail, loginPassword)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    user!!.updatePassword(newPassword)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val usersReference = dbReference.getReference("users")
+                                usersReference.child(usersList[position].uid.toString()).child("password").setValue(newPassword)
+                                firebaseAuth.signOut()
+                                firebaseAuth.signInWithEmailAndPassword(mainUserEmail, mainUserPassword)
+                                    .addOnCompleteListener {
+                                        if (it.isSuccessful) {
+                                            binding.clLoadingUsers.visibility = View.GONE
+                                            Toast.makeText(this@UsersActivity, "Password Changed Successfully!!.", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                            } else {
+                                binding.clLoadingUsers.visibility = View.GONE
+                                Toast.makeText(this@UsersActivity, "Password Change Failed!!.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    binding.clLoadingUsers.visibility = View.GONE
+                    Toast.makeText(this@UsersActivity, "Password Change Failed!!.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun getMainUserIndex(): Int {
+        var userPosition = -1
+
+        usersList.forEachIndexed { index, userGet ->
+            if (userGet.name == LoggedInUser.userName) {
+                userPosition = index
+            }
+        }
+
+        return userPosition
     }
 
     @SuppressLint("SetTextI18n")
